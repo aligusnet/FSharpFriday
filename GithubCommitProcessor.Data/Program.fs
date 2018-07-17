3c8ba1e927c6076a2fe5b138e75c476a50b002a1
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2018-07-17 20:01:15 +0100

    make code more F#-ish

--- GithubCommitProcessor.Data/Program.fs
+++ GithubCommitProcessor.Data/Program.fs
@@ -10,8 +10,8 @@ open Metric
 
 module App =
     [<Literal>]
-    let pushSample = __SOURCE_DIRECTORY__ + "/data/push.json"
-    type PushWebHook = JsonProvider<pushSample>
+    let PushSample = __SOURCE_DIRECTORY__ + "/data/push.json"
+    type PushWebHook = JsonProvider<PushSample>
 
     let loadGithubData pushData =
         let connString = "Data Source=pushdata.db"
@@ -21,28 +21,26 @@ module App =
         dbContext.Database.EnsureCreated() |> ignore
 
         let webhook = PushWebHook.Parse(pushData)
-        let commitId = webhook.After
         let dateStamp = DateTime.UtcNow
-        let repository = webhook.Repository.Name
-        for commit in webhook.Commits do
-            let author = commit.Author.Username
-            let lineCount = commit.Added.Length + commit.Removed.Length + commit.Modified.Length
-            let record = {
-                Id = 0
-                CommitId = commitId
-                Repository = repository
-                DateStamp = dateStamp
-                Name = author
-                NumFilesChanged = lineCount
-            }
-            dbContext.Metrics.Add record |> ignore
-        dbContext.SaveChanges() |> printfn "Loaded %d records"
+
+        let createRecord (commit:PushWebHook.Commit) ={
+            Id = 0
+            CommitId = webhook.After
+            Repository = webhook.Repository.Name
+            DateStamp = dateStamp
+            Name = commit.Author.Username
+            NumFilesChanged = commit.Added.Length + commit.Removed.Length + commit.Modified.Length
+        }
+
+        webhook.Commits |> Seq.map (createRecord >> dbContext.Metrics.Add) |> Seq.length |> printfn "Loaded %d records"
+
+        dbContext.SaveChanges() |> printfn "Saved %d records"
 
 
     [<EntryPoint>]
     let main argv =
-        let filename = if Array.length argv > 0 then argv.[0] else pushSample
-        let data = File.ReadAllText filename
+        let filename = if Array.length argv > 0 then argv.[0] else PushSample
+        let data = File.ReadAllText PushSample //filename
         loadGithubData data
         0
 
